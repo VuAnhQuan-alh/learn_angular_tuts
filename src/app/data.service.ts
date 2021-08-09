aef8b0503a2e0bd2870b84324224bb154a44209e
VuAnhQuan-alh <quanva2521@gmail.com>
2021-08-09 08:13:22 +0700

    1 file modified, 2 deleted

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/dist/types';
+import { Observable } from 'rxjs';
 import { TypeMovies } from './data.type';
 
 @Injectable()
